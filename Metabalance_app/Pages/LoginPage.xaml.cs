5090837d1d02a5babfc9ee00db70cb2a3697b830
Borjarovadasz <farkas.nandor.meszaros@gmail.com>
2025-12-04 13:06:55 +0100

    Haladunk 2

---
+++ Metabalance_app/Pages/LoginPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Metabalance_app.Pages
+{
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Metabalance_app/Pages/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Metabalance_app.Pages
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Bejelentkezes_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new LoginPage());
+        }
+    }
+}
